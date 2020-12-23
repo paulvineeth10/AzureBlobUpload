8c22ff6719965019f90a4cbf29d254927e1774f4
paulvineeth10 <76525063+paulvineeth10@users.noreply.github.com>
2020-12-23 19:40:51 +0530

    Added files via upload

---
+++ uploadBlob/Controllers/UploadController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using uploadBlob.Services;
+
+namespace uploadBlob.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        private IBlobServicee _blobService;
+
+        public UploadController(IBlobServicee blobService)
+        {
+            _blobService = blobService;
+        }
+
+        [HttpPost(""), DisableRequestSizeLimit]
+        public async Task<ActionResult> UploadProfilePicture()
+        {
+            IFormFile file = Request.Form.Files[0];
+            if (file == null)
+            {
+                return BadRequest();
+            }
+
+            var result = await _blobService.UploadFileBlobAsync(
+                    "firstcontainer",
+                    file.OpenReadStream(),
+                    file.ContentType,
+                    file.FileName);
+
+            var toReturn = result.AbsoluteUri;
+
+            return Ok(new { path = toReturn });
+        }
+    }
+}
+
