9ba5b5382fcf4b827e2e517d8770537b59a96e2a
paulvineeth10 <76525063+paulvineeth10@users.noreply.github.com>
2020-12-23 19:43:55 +0530

    Add files via upload

---
+++ uploadBlob/Services/IBlobServicee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace uploadBlob.Services
+{
+    public interface IBlobServicee
+    {
+        public Task<Uri> UploadFileBlobAsync(string blobContainerName, Stream content, string contentType, string fileName);
+
+
+
+    }
+}
